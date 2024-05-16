1a4cb53dd145e3be1184630ab2e9a7f8143b4fb2
Daram Bae <dabae@c3r2p6.42perpignan.fr>
2024-05-16 14:32:54 +0200

    editted get_token function

--- src/parsing.c
+++ src/parsing.c
@@ -13,30 +13,49 @@ bool	peek(char **start_str, char *end_str, char *c)
 	return (*tmp && ft_strchr(c , *tmp));
 }
 
-bool	get_token(char **start_line, char **end_line, char **start_t, char **end_t)
+int	get_token(char **start_line, char *end_line, char **start_t, char **end_t)
 {
 	char	*whitespace;
 	char	*symbols;
 	char	*cur;
-	int		token;
+	int		res;
 
 	whitespace = " \t\n\v\r";
 	symbols = "$|><";
 	cur = *start_line;
-	while (*cur && ft_strchr(whitespace, *cur))
+	while (cur < end_line && ft_strchr(whitespace, *cur))
 		cur++;
-	if (!*cur)
-		return (false);
-	*start_t = cur;
-	if (ft_strchr(symbols, *cur))
+	if (start_t)
+		*start_t = cur;
+	res = *cur;
+	if (*cur == '|')
+		res = '-';
+	else if (*cur == '<')
 	{
-		*end_t = cur + 1;
-		*start_line = cur + 1;
-		return (true);
-	}
-	while (*cur && !ft_strchr(whitespace, *cur) && !ft_strchr(symbols, *cur))
 		cur++;
-	*end_t = cur;
-	*start_line = cur;
-	return (true);
+		if (*cur == '<')
+			res = '{'; //For <<
+		else
+			res = '['; //For <
+	}
+	else if (*cur == '>')
+	{
+		cur++;
+		if (*cur == '>')
+			res = '}'; //For >>
+		else
+			res = ']'; //For >
+	}
+	else
+	{
+		res = 'a'; //For any other argv including CMD, ARG, ETC,,,
+		while (cur < end_line && !ft_strchr(whitespace, *cur) && !ft_strchr(symbols, *cur))
+			cur++;
+	}
+	if (end_t)
+		*end_t = cur;
+	while (cur < end_line && ft_strchr(whitespace, *cur))
+		cur++;
+	*start_line = cur; //update where to start scanning
+	return (res);
 }
