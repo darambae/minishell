7244bb355446d2441ab07636e2a4a69f274b793b
Daram Bae <dabae@c3r2p6.42perpignan.fr>
2024-05-16 13:28:25 +0200

    created peek function

--- src/parsing.c
+++ src/parsing.c
@@ -1,6 +1,18 @@
 
 #include "../minishell.h"
 
+//Check a str through and skip whitespace. Set start_str on the non-whitespace char and check if it's a token.
+bool	peek(char **start_str, char *end_str, char *c)
+{
+	char	*tmp;
+
+	tmp = *start_str;
+	while (tmp < end_str && ft_strchr(" \t\n\v\r", *tmp))
+		tmp++;
+	*start_str = tmp;
+	return (*tmp && ft_strchr(c , *tmp));
+}
+
 bool	get_token(char **start_line, char **end_line, char **start_t, char **end_t)
 {
 	char	*whitespace;
@@ -9,7 +21,7 @@ bool	get_token(char **start_line, char **end_line, char **start_t, char **end_t)
 	int		token;
 
 	whitespace = " \t\n\v\r";
-	symbols = "$\"'|><";
+	symbols = "$|><";
 	cur = *start_line;
 	while (*cur && ft_strchr(whitespace, *cur))
 		cur++;
