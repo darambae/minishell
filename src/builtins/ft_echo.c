e59a0fc52fbf3cd14ef7d5c27d30854eb69b2657
Kelly Brener-Raffalli <kbrener-@c3r1p3.42perpignan.fr>
2024-05-27 16:31:07 +0200

    echo_quote_parsing

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -6,7 +6,9 @@ void	ft_echo(char **cmd)
 	char	*s;
 
 	s = check_cmd(cmd);
-	ft_printf("%s", s);
+	if (s == NULL)
+		exit(printf("\n"));
+	exit(printf("%s", s));
 }
 //clean the string after the cmd echo
 char	*check_cmd(char **cmd)
@@ -27,7 +29,7 @@ char	*check_cmd(char **cmd)
 		if (*s == '-')
 		{
 			s++;
-			if (*s == 'n')
+			if (*s == 'n' && (*(s + 1) == " " || *(s + 1) == "\0"))
 				end = "\0";
 		}
 		while (*s == '\'' || *s =='\"')
@@ -50,6 +52,46 @@ char	*check_cmd(char **cmd)
 		result = del_multiple_char(result, s, " ");
 	}
 }
+char	*check_quote(char *s)
+{
+	char	*result;
+	int	i;
 
-char	*del_multiple_char(char *result, char *s, char c)
-{}
+	i = 0;
+	result = malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (!result)
+		return (NULL);
+	while (s)
+	{
+		if (*s == "\'")
+			keep_between_quote(result, &s, "\'", &i);
+		else if (*s == "\"")
+			keep_between_quote(result, &s, "\"", &i);
+		else if (*s && *s != "\"" && *s != "\'")
+			result[i] = *s++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+void	keep_between_quote(char *result, char **argv, char c, int *i)
+{
+	char	*s;
+
+	s = *argv;
+	s++;
+	while (*s && *s != c)
+	{
+		if (*s == "$")
+		{
+			result[*i] = "\0";
+			result = get_path(&s, result);
+		}
+		result[*i++] = s++;
+		if (!*s)
+			ft_error("single quote not closed");
+		s++;
+	}
+	*argv = s;
+}
+
