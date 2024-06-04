e2b66fd2e070cd3489352263c217eabac1f47953
Daram Bae <dabae@c3r1p5.42perpignan.fr>
2024-06-04 16:25:21 +0200

    unset & export done

--- src/builtins/ft_export.c
+++ src/builtins/ft_export.c
@@ -2,59 +2,79 @@
 
 static bool check_export_syntax(char **cmd)
 {
-    int i;
+	int i;
 
-    i = 0;
-    if (cmd[1])
-    {
-        if (ft_isdigit(cmd[1][0]))
-            return (false);
-        while (cmd[1][i] && (ft_isalpha(cmd[1][i]) || ft_isdigit(cmd[1][i]) || \
-            ft_strchr("_" ,cmd[1][i])))
-            i++;
-        if (!cmd[1][i] || cmd[1][i] == '=')
-            return (true);
-    }
-    return (false);
+	i = 0;
+	if (cmd[1])
+	{
+		if (ft_isdigit(cmd[1][0]))
+			return (false);
+		while (cmd[1][i] && (ft_isalpha(cmd[1][i]) || ft_isdigit(cmd[1][i]) || \
+			ft_strchr("_" ,cmd[1][i])))
+			i++;
+		if (!cmd[1][i] || cmd[1][i] == '=')
+			return (true);
+	}
+	return (false);
 }
 
-static char **new_arr(char **prev, char *new)
+static char **new_arr(char *new, int len_env, int len_name)
 {
-    char    **new_env;
-    int	len_env;
+	char    **new_env;
+	int		i;
+	int		j;
 
-	len_env = 0;
-	while (g_param->env_variables[len_env])
-		len_env++;
-    new_env = (char **)malloc(sizeof(char *) * (len_env + 1));
+	new_env = (char **)malloc(sizeof(char *) * (len_env + 1));
 	if (!new_env)
 		return NULL;
+	i = 0;
+	j = 0;
+	while (i < len_env)
+	{
+		if (!g_param->env_variables[i])
+		{
+			new_env[j] = ft_strdup(new);
+			break ;
+		}
+		if (!ft_strncmp(g_param->env_variables[i], new, len_name))
+			new_env[j] = ft_strdup(new);
+		else
+			new_env[j] = ft_strdup(g_param->env_variables[i]);
+		j++;
+		i++;
+	}
+	ft_free_tab(g_param->env_variables);
+	return (new_env);
 }
 
 void    ft_export(t_execcmd *cmd)
 {
-    int     i;
-    int     j;
+	int     len_name;
+	int     j;
+	int		len_env;
 
-    i = 0;
-    if (check_export_syntax(cmd->argv) == false)
-    {
-        errno = 1;
-        perror("export syntax error");
-        g_param->exit_status = 1;
-        return ;
-    }
-    while (cmd->argv[1][i] != '=' && cmd->argv[1][i])
-        i++;
-    j = 0;
-    while (g_param->env_variables[j] && ft_strncmp(g_param->env_variables[j], cmd->argv[1], i))
-        j++;
-    if (cmd->argv[1][i] == '=')
-    {
-        ft_realloc(g_param->env_variables[j], ft_strlen(cmd->argv[1]));
-        g_param->env_variables[j] = ft_strdup(cmd->argv[1]);
-    }
-        //tmp = g_param->env_variables[j];
-        //free(tmp);
-        
+	len_env = 0;
+	while (g_param->env_variables[len_env])
+		len_env++;
+	len_name = 0;
+	if (check_export_syntax(cmd->argv) == false)
+	{
+		errno = 1;
+		perror("export syntax error");
+		g_param->exit_status = 1;
+		return ;
+	}
+	while (cmd->argv[1][len_name] != '=' && cmd->argv[1][len_name])
+		len_name++;
+	j = 0;
+	while (g_param->env_variables[j] && ft_strncmp(g_param->env_variables[j], cmd->argv[1], len_name))
+		j++;
+	if (cmd->argv[1][len_name] == '=')
+	{
+		if (!g_param->env_variables[j])
+			len_env++;
+		g_param->env_variables = new_arr(cmd->argv[1], len_env, len_name);
+	}
+		//tmp = g_param->env_variables[j];
+		//free(tmp);
 }
