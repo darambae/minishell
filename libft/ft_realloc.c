a83b0fb847665b2f94e517ee7e2132859926aa39
Daram Bae <baedaram@gmail.com>
2024-06-06 07:17:00 +0000

    .

--- libft/ft_realloc.c
+++ libft/ft_realloc.c
@@ -6,24 +6,25 @@
 /*   By: dabae <dabae@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 17:20:24 by dabae             #+#    #+#             */
-/*   Updated: 2024/06/04 08:42:35 by dabae            ###   ########.fr       */
+/*   Updated: 2024/06/06 07:10:49 by dabae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 /*To reallocate memory but only smaller than the existing size(shrinking)*/
-void    *ft_realloc(void *p, size_t new_size)
+void	*ft_realloc(void *p, size_t new_size)
 {
-    char    *new_p;
-    size_t  cur_size;
+	char	*new_p;
+	size_t	cur_size;
 
-    if (!p)
-        return (malloc(new_size));
-    cur_size = sizeof(p);
-    if (new_size <= cur_size)
-        return (p);
-    new_p = malloc(new_size);
-    ft_memcpy(p, new_p, cur_size);
-    free(p);
-    return (new_p);
+	if (!p)
+		return (malloc(new_size));
+	cur_size = sizeof(p);
+	if (new_size <= cur_size)
+		return (p);
+	new_p = malloc(new_size);
+	new_p = NULL;
+	ft_memcpy(p, new_p, cur_size);
+	free(p);
+	return (new_p);
 }
